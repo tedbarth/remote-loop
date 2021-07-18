4a7ed9a0e1c9afa963faa1047b7ac4ad70612343
Ted <me@luftis-world.de>
2021-07-18 13:49:14 +0200

    Formatted code

--- remote-loop.sh
+++ remote-loop.sh
@@ -11,8 +11,8 @@ clearLine='\e[1A\e[K'
 # CODE
 mkdir -p "$LOG_DIR"
 
-function save_db () {
-  declare -p database > "$DATABASE_FILE"
+function save_db() {
+  declare -p database >"$DATABASE_FILE"
 }
 
 function timestamp() {
@@ -21,50 +21,47 @@ function timestamp() {
 
 # Read update db and create file if not existing
 declare -A database
-if [ -f "$DATABASE_FILE" ];then
-  source "$DATABASE_FILE"
+if [ -f "$DATABASE_FILE" ]; then
+  source "${DATABASE_FILE}"
 else
   save_db
 fi
 
-i=0;
-while true
-do
-  i=$((i+1))
+i=0
+while true; do
+  i=$((i + 1))
   echo -e "\n--- Loop #${i} ---"
 
-	# Read hosts file
-	hosts=()
-	if [ -f "$HOSTS_FILE" ];then
-    readarray -t hosts < "$HOSTS_FILE"
+  # Read hosts file
+  hosts=()
+  if [ -f "$HOSTS_FILE" ]; then
+    readarray -t hosts <"$HOSTS_FILE"
   fi
 
   # Loop over hosts
-  for host in "${hosts[@]}"
-  do
+  for host in "${hosts[@]}"; do
     # Extract hostname and port
-    IFS=' ' read -r hostname_port host_interval_seconds command <<< "$host"
-    IFS=: read -r hostname port <<< "$hostname_port"
-
+    IFS=' ' read -r hostname_port host_interval_seconds command <<<"$host"
+    IFS=: read -r hostname port <<<"$hostname_port"
 
     # Ignore hosts entry if empty line or starts with # (comment)
     if [[ "$hostname" == "" ]] || [[ "$hostname" == \#* ]]; then
-      continue;
+      continue
     fi
 
     now=$(timestamp)
-    if (( database[$hostname] < $now - $host_interval_seconds  )); then
+    if ((database[$hostname] < $now - $host_interval_seconds)); then
       echo "$hostname: Checking availability…"
       if nc -z $hostname ${port:-22} 2>/dev/null; then
-          echo -e "${clearLine}${hostname}: Executing command…"
-          ( # Subshell to not stop due to error
-            eval $command > "$LOG_DIR/$hostname.txt" 2>&1
-          )
-          database[$hostname]=$now
-          save_db
-          echo -e "${clearLine}$hostname: ✓"
+        echo -e "${clearLine}${hostname}: Executing command…"
+        (# Subshell to not stop due to error
+          eval $command >"$LOG_DIR/$hostname.txt" 2>&1
+        )
+        database[$hostname]=$now
+        save_db
+        echo -e "${clearLine}$hostname: ✓"
       else
-          echo -e "${clearLine}$hostname: ✗"
+        echo -e "${clearLine}$hostname: ✗"
       fi
     else
       echo "$hostname: ⧖"
@@ -73,6 +70,5 @@ do
   echo -e "--- Loop end ---"
 
   # Wait some time
-	sleep $AVAILABILITY_CHECK_INTERVAL_SECONDS
+  sleep $AVAILABILITY_CHECK_INTERVAL_SECONDS
 done
-
